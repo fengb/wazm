55a855786535b190f18dde11ea63139965f782a9
Benjamin Feng <benjamin.feng@glassdoor.com>
2020-01-23 23:46:03 -0600

    Introduce synthetic union "Arg"

--- src/core.zig
+++ src/core.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const Op = @import("op.zig");
 
 pub const Module = struct {
     arena: *std.heap.ArenaAllocator,
@@ -37,89 +38,7 @@ pub const Module = struct {
     };
 
     pub const Instr = struct {
-        op: Op,
-        arg: union(enum) {
-            none: void,
-            type_: i8,
-            u32: u32,
-            u64: u64,
-            f32: f32,
-            f64: f64,
-            mem: struct {
-                offset: u32,
-                align_: u32,
-            },
-        },
+        op: u8,
+        arg: Op.Arg,
     };
-};
-
-pub const Op = enum(u8) {
-    Unreachable = 0x00,
-    Nop = 0x01,
-    Block = 0x02,
-    Loop = 0x03,
-    If = 0x04,
-    Else = 0x05,
-    // Try = 0x06,
-    // Catch = 0x07,
-    // Throw = 0x08,
-    // Rethrow = 0x09,
-    // BrOnExn = 0x0A,
-    End = 0x0B,
-    Br = 0x0C,
-    BrIf = 0x0D,
-    BrTable = 0x0E,
-    BrReturn = 0x0F,
-
-    Call = 0x10,
-    CallIndirect = 0x11,
-    // ReturnCall = 0x12,
-    // ReturnCallIndirect = 0x13,
-    // 0x14
-    // 0x15
-    // 0x16
-    // 0x17
-    // 0x18
-    // 0x19
-    Drop = 0x1A,
-    Select = 0x1B,
-    // SelectT = 0x1C,
-    // 0x1D
-    // 0x1E
-    // 0x1F
-
-    LocalGet = 0x20,
-    LocalSet = 0x21,
-    LocalTee = 0x22,
-    GlobalGet = 0x23,
-    GlobalSet = 0x24,
-    // TableGet = 0x25,
-    // TableSet = 0x26,
-    // 0x27
-    I32Load = 0x28,
-    I64Load = 0x29,
-    F32Load = 0x2A,
-    F64Load = 0x2B,
-    I32Load8S = 0x2C,
-    I32Load8U = 0x2D,
-    I32Load16S = 0x2E,
-
-    I64Load8S = 0x30,
-    I64Load8U = 0x31,
-    I64Load16S = 0x32,
-    I64Load16U = 0x33,
-    I64Load32S = 0x34,
-    I64Load32U = 0x35,
-    I32Store = 0x36,
-    I64Store = 0x37,
-    F32Store = 0x38,
-    F64Store = 0x39,
-    I32Store8 = 0x3A,
-    I32Store16 = 0x3B,
-    I64Store8 = 0x3C,
-    I64Store16 = 0x3D,
-    I64Store32 = 0x3E,
-    MemorySize = 0x3F,
-
-    _,
 };
--- src/op.zig
+++ src/op.zig
@@ -1,30 +1,67 @@
 const std = @import("std");
 const builtin = @import("builtin");
 
-pub const Type = enum {
+pub const StackChange = enum {
+    Void,
     I32,
     I64,
     F32,
     F64,
 
-    fn fromRaw(comptime T: type) ?Type {
+    fn fromRaw(comptime T: type) StackChange {
         return switch (T) {
-            i32 => Type.I32,
-            i64 => Type.I64,
-            f32 => Type.F32,
-            f64 => Type.F64,
-            void => null,
+            void => .Void,
+            i32 => .I32,
+            i64 => .I64,
+            f32 => .F32,
+            f64 => .F64,
             else => @compileError("Unsupported type:" ++ @typeName(T)),
         };
     }
+};
+
+pub const Arg = packed union {
+    b1: u8,
+    b4: [4]u8,
+    b5: [5]u8,
+    b8: [8]u8,
+    _pad: u64,
+
+    pub const None = packed union {
+        const bytes = 0;
+        _pad: u64,
+    };
+    pub const Type = enum(u64) {
+        const bytes = 1;
+        Void = 0x40,
+        I32 = 0x7F,
+        I64 = 0x7E,
+        F32 = 0x7D,
+        F64 = 0x7C,
+    };
+    pub const I32 = packed union {
+        const bytes = 4;
+        data: u32,
+        _pad: u64,
+    };
+    pub const I32z = packed union {
+        const bytes = 5;
+        data: u32,
+        _pad: u64,
+    };
+    pub const Mem = packed struct {
+        const bytes = 8;
+        offset: u32,
+        align_: u32,
+    };
 };
 
 const Meta = struct {
     code: u8,
     name: []const u8,
     arg_bytes: u8,
-    push: ?Type,
-    pop: [2]?Type,
+    push: StackChange,
+    pop: [2]StackChange,
 
     fn lessThan(lhs: Meta, rhs: Meta) bool {
         return std.mem.lessThan(u8, lhs.name, rhs.name);
@@ -64,10 +101,10 @@ pub const sparse = blk: {
         result[i] = .{
             .code = std.fmt.parseInt(u8, decl.name[2..4], 16) catch unreachable,
             .name = decl.name[5..],
-            .arg_bytes = @sizeOf(arg_arg.arg_type.?),
-            .push = Type.fromRaw(decl.data.Fn.return_type),
+            .arg_bytes = arg_arg.arg_type.?.bytes,
+            .push = StackChange.fromRaw(decl.data.Fn.return_type),
             .pop = switch (@typeInfo(arg_pop.arg_type.?)) {
-                .Void, .Int, .Float => .{ Type.fromRaw(arg_pop.arg_type.?), null },
+                .Void, .Int, .Float => .{ StackChange.fromRaw(arg_pop.arg_type.?), .Void },
                 else => @compileError("Unsupported pop type: " ++ @typeName(arg_pop.arg_type.?)),
             },
         };
@@ -79,7 +116,7 @@ pub const sparse = blk: {
 };
 
 pub const all = blk: {
-    const uninit = Meta{ .code = 0xAA, .name = "ILLEGAL", .arg_bytes = 0, .pop = .{ null, null }, .push = null };
+    const uninit = Meta{ .code = 0xAA, .name = "ILLEGAL", .arg_bytes = 0, .pop = .{ .Void, .Void }, .push = .Void };
     var result = [_]Meta{uninit} ** 256;
     for (result) |*meta, i| {
         meta.code = i;
@@ -125,43 +162,43 @@ fn publicFunctions(comptime T: type) []builtin.TypeInfo.Declaration {
 test "ops" {
     const nop = byName("nop").?;
     std.testing.expectEqual(nop.arg_bytes, 0);
-    std.testing.expectEqual(nop.push, null);
-    std.testing.expectEqual(nop.pop[0], null);
-    std.testing.expectEqual(nop.pop[1], null);
+    std.testing.expectEqual(nop.push, .Void);
+    std.testing.expectEqual(nop.pop[0], .Void);
+    std.testing.expectEqual(nop.pop[1], .Void);
 
     const i32_load = byName("i32.load").?;
     std.testing.expectEqual(i32_load.arg_bytes, 8);
-    std.testing.expectEqual(i32_load.push, Type.I32);
-    std.testing.expectEqual(i32_load.pop[0], Type.I32);
-    std.testing.expectEqual(i32_load.pop[1], null);
+    std.testing.expectEqual(i32_load.push, .I32);
+    std.testing.expectEqual(i32_load.pop[0], .I32);
+    std.testing.expectEqual(i32_load.pop[1], .Void);
 }
 
 const Impl = struct {
     const Context = struct {};
 
-    pub fn @"0x00 unreachable"(ctx: Context, arg: void, pop: void) void {}
+    pub fn @"0x00 unreachable"(ctx: Context, arg: Arg.None, pop: void) void {}
 
-    pub fn @"0x01 nop"(ctx: Context, arg: void, pop: void) void {}
+    pub fn @"0x01 nop"(ctx: Context, arg: Arg.None, pop: void) void {}
 
-    pub fn @"0x02 block"(ctx: Context, arg: ?Type, pop: void) void {}
+    pub fn @"0x02 block"(ctx: Context, arg: Arg.Type, pop: void) void {}
 
-    pub fn @"0x03 loop"(ctx: Context, arg: ?Type, pop: void) void {}
+    pub fn @"0x03 loop"(ctx: Context, arg: Arg.Type, pop: void) void {}
 
-    pub fn @"0x04 if"(ctx: Context, arg: ?Type, pop: i32) void {}
+    pub fn @"0x04 if"(ctx: Context, arg: Arg.Type, pop: i32) void {}
 
-    pub fn @"0x05 else"(ctx: Context, arg: void, pop: void) void {}
+    pub fn @"0x05 else"(ctx: Context, arg: Arg.None, pop: void) void {}
 
-    pub fn @"0x0B end"(ctx: Context, arg: void, pop: void) void {}
+    pub fn @"0x0B end"(ctx: Context, arg: Arg.None, pop: void) void {}
 
-    pub fn @"0x0C br"(ctx: Context, arg: void, pop: void) void {}
+    pub fn @"0x0C br"(ctx: Context, arg: Arg.None, pop: void) void {}
 
-    pub fn @"0x0D br_if"(ctx: Context, arg: u32, pop: void) void {}
+    pub fn @"0x0D br_if"(ctx: Context, arg: Arg.I32, pop: void) void {}
 
-    pub fn @"0x0E br_table"(ctx: Context, arg: [2]u32, pop: void) void {}
+    pub fn @"0x0E br_table"(ctx: Context, arg: Arg.Mem, pop: void) void {}
 
-    pub fn @"0x0F return"(ctx: Context, arg: void, pop: void) void {}
+    pub fn @"0x0F return"(ctx: Context, arg: Arg.None, pop: void) void {}
 
-    pub fn @"0x28 i32.load"(ctx: Context, arg: [2]u32, pop: i32) i32 {
+    pub fn @"0x28 i32.load"(ctx: Context, arg: Arg.Mem, pop: i32) i32 {
         return 0;
     }
 };
