ea3ab641a4c6c459fe8d0b07b08a22c99016abee
Benjamin Feng <benjamin.feng@glassdoor.com>
2020-09-14 21:48:51 -0500

    Test out ~all~ some of the maths

--- src/func/basic.zig
+++ src/func/basic.zig
@@ -50,3 +50,147 @@ test "i32 math" {
         std.testing.expectEqual(@as(i32, 20), result.?.I32);
     }
 }
+
+test "i64 math" {
+    var fbs = std.io.fixedBufferStream(
+        \\(module
+        \\  (func (result i64)
+        \\    i64.const 40
+        \\    i64.const 2
+        \\    i64.add)
+        \\  (func (result i64)
+        \\    i64.const 40
+        \\    i64.const 2
+        \\    i64.sub)
+        \\  (func (result i64)
+        \\    i64.const 40
+        \\    i64.const 2
+        \\    i64.mul)
+        \\  (func (result i64)
+        \\    i64.const 40
+        \\    i64.const 2
+        \\    i64.div_s)
+        \\  (export "add" (func 0))
+        \\  (export "sub" (func 1))
+        \\  (export "mul" (func 2))
+        \\  (export "div" (func 3)))
+    );
+    var module = try Wat.parse(std.testing.allocator, fbs.reader());
+    defer module.deinit();
+
+    var instance = try module.instantiate(std.testing.allocator, struct {});
+    defer instance.deinit();
+
+    {
+        const result = try instance.call("add", &[0]Instance.Value{});
+        std.testing.expectEqual(@as(i64, 42), result.?.I64);
+    }
+    {
+        const result = try instance.call("sub", &[0]Instance.Value{});
+        std.testing.expectEqual(@as(i64, 38), result.?.I64);
+    }
+    {
+        const result = try instance.call("mul", &[0]Instance.Value{});
+        std.testing.expectEqual(@as(i64, 80), result.?.I64);
+    }
+    {
+        const result = try instance.call("div", &[0]Instance.Value{});
+        std.testing.expectEqual(@as(i64, 20), result.?.I64);
+    }
+}
+
+test "f32 math" {
+    var fbs = std.io.fixedBufferStream(
+        \\(module
+        \\  (func (result f32)
+        \\    f32.const 40
+        \\    f32.const 2
+        \\    f32.add)
+        \\  (func (result f32)
+        \\    f32.const 40
+        \\    f32.const 2
+        \\    f32.sub)
+        \\  (func (result f32)
+        \\    f32.const 40
+        \\    f32.const 2
+        \\    f32.mul)
+        \\  (func (result f32)
+        \\    f32.const 40
+        \\    f32.const 2
+        \\    f32.div)
+        \\  (export "add" (func 0))
+        \\  (export "sub" (func 1))
+        \\  (export "mul" (func 2))
+        \\  (export "div" (func 3)))
+    );
+    var module = try Wat.parse(std.testing.allocator, fbs.reader());
+    defer module.deinit();
+
+    var instance = try module.instantiate(std.testing.allocator, struct {});
+    defer instance.deinit();
+
+    {
+        const result = try instance.call("add", &[0]Instance.Value{});
+        std.testing.expectEqual(@as(f32, 42), result.?.F32);
+    }
+    {
+        const result = try instance.call("sub", &[0]Instance.Value{});
+        std.testing.expectEqual(@as(f32, 38), result.?.F32);
+    }
+    {
+        const result = try instance.call("mul", &[0]Instance.Value{});
+        std.testing.expectEqual(@as(f32, 80), result.?.F32);
+    }
+    {
+        const result = try instance.call("div", &[0]Instance.Value{});
+        std.testing.expectEqual(@as(f32, 20), result.?.F32);
+    }
+}
+
+test "f64 math" {
+    var fbs = std.io.fixedBufferStream(
+        \\(module
+        \\  (func (result f64)
+        \\    f64.const 1
+        \\    f64.const 2
+        \\    f64.add)
+        \\  (func (result f64)
+        \\    f64.const 1
+        \\    f64.const 2
+        \\    f64.sub)
+        \\  (func (result f64)
+        \\    f64.const 1
+        \\    f64.const 2
+        \\    f64.mul)
+        \\  (func (result f64)
+        \\    f64.const 1
+        \\    f64.const 2
+        \\    f64.div)
+        \\  (export "add" (func 0))
+        \\  (export "sub" (func 1))
+        \\  (export "mul" (func 2))
+        \\  (export "div" (func 3)))
+    );
+    var module = try Wat.parse(std.testing.allocator, fbs.reader());
+    defer module.deinit();
+
+    var instance = try module.instantiate(std.testing.allocator, struct {});
+    defer instance.deinit();
+
+    {
+        const result = try instance.call("add", &[0]Instance.Value{});
+        std.testing.expectEqual(@as(f64, 3), result.?.F64);
+    }
+    {
+        const result = try instance.call("sub", &[0]Instance.Value{});
+        std.testing.expectEqual(@as(f64, -1), result.?.F64);
+    }
+    {
+        const result = try instance.call("mul", &[0]Instance.Value{});
+        std.testing.expectEqual(@as(f64, 2), result.?.F64);
+    }
+    {
+        const result = try instance.call("div", &[0]Instance.Value{});
+        std.testing.expectEqual(@as(f64, 0.5), result.?.F64);
+    }
+}
