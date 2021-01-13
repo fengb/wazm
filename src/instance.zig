c66103367e66fa011fe3be03c18101923853b23b
Benjamin Feng <benjamin.feng@glassdoor.com>
2021-01-12 22:30:54 -0600

    Convert giant generic instance into a wrapped function pointer

--- src/instance.zig
+++ src/instance.zig
@@ -4,7 +4,18 @@ const Module = @import("module.zig");
 const Op = @import("op.zig");
 const Execution = @import("execution.zig");
 
-pub fn init(module: *Module, allocator: *std.mem.Allocator, comptime Imports: type) !Instance(Imports) {
+const Instance = @This();
+
+module: *Module,
+memory: []u8,
+allocator: *std.mem.Allocator,
+exports: std.StringHashMap(Export),
+funcs: []Func,
+
+// TODO: revisit if wasm ever becomes multi-threaded
+mutex: std.Mutex,
+
+pub fn init(module: *Module, allocator: *std.mem.Allocator, comptime Imports: type) !Instance {
     //    var import_funcs = try std.ArrayList(Instance.ImportFunc).initCapacity(allocator, module.imports.len);
     //    errdefer import_funcs.deinit();
     //    for (module.imports) |import| cont: {
@@ -45,10 +56,7 @@ pub fn init(module: *Module, allocator: *std.mem.Allocator, comptime Imports: ty
                 .result = func_type.return_type,
                 .locals = &[0]Module.Type.Value{},
                 .kind = .{
-                    .imported = .{
-                        .module = import.module,
-                        .field = import.field,
-                    },
+                    .imported = ImportedFunc.init(Imports, import.module, import.field),
                 },
             });
         }
@@ -66,7 +74,7 @@ pub fn init(module: *Module, allocator: *std.mem.Allocator, comptime Imports: ty
         });
     }
 
-    return Instance(Imports){
+    return Instance{
         .module = module,
         .mutex = std.Mutex{},
         .memory = try allocator.alloc(u8, 65536),
@@ -76,194 +84,125 @@ pub fn init(module: *Module, allocator: *std.mem.Allocator, comptime Imports: ty
     };
 }
 
-pub fn Instance(comptime Imports: type) type {
-    return struct {
-        const Self = @This();
+pub fn deinit(self: *Instance) void {
+    self.allocator.free(self.funcs);
+    self.allocator.free(self.memory);
+    self.exports.deinit();
+    self.* = undefined;
+}
 
-        module: *Module,
-        memory: []u8,
-        allocator: *std.mem.Allocator,
-        exports: std.StringHashMap(Export),
-        funcs: []Func,
+pub fn call(self: *Instance, name: []const u8, params: anytype) !?Value {
+    const lock = self.mutex.acquire();
+    defer lock.release();
 
-        // TODO: revisit if wasm ever becomes multi-threaded
-        mutex: std.Mutex,
+    const exp = self.exports.get(name) orelse return error.ExportNotFound;
+    if (exp != .Func) {
+        return error.ExportNotAFunction;
+    }
 
-        pub fn deinit(self: *Self) void {
-            self.allocator.free(self.funcs);
-            self.allocator.free(self.memory);
-            self.exports.deinit();
-            self.* = undefined;
-        }
+    const func_id = exp.Func;
+    const func = self.funcs[func_id];
+    if (params.len != func.params.len) {
+        return error.TypeSignatureMismatch;
+    }
 
-        pub fn call(self: *Self, name: []const u8, params: anytype) !?Value {
-            const lock = self.mutex.acquire();
-            defer lock.release();
+    var converted_params: [params.len]Op.Fixval = undefined;
+    inline for ([_]void{{}} ** params.len) |_, i| {
+        const param_type: Module.Type.Value = switch (@TypeOf(params[i])) {
+            i32, u32 => .I32,
+            i64, u64 => .I64,
+            f32 => .F32,
+            f64 => .F64,
+            else => @compileError("Unsupported type"),
+        };
+        if (param_type != func.params[i]) return error.TypeSignatureMismatch;
 
-            const exp = self.exports.get(name) orelse return error.ExportNotFound;
-            if (exp != .Func) {
-                return error.ExportNotAFunction;
-            }
+        converted_params[i] = switch (@TypeOf(params[i])) {
+            i32 => .{ .I32 = params[i] },
+            i64 => .{ .I64 = params[i] },
+            u32 => .{ .U32 = params[i] },
+            u64 => .{ .U64 = params[i] },
+            f32 => .{ .F32 = params[i] },
+            f64 => .{ .F64 = params[i] },
+            else => @compileError("Unsupported type"),
+        };
+    }
 
-            const func_id = exp.Func;
-            const func = self.funcs[func_id];
-            if (params.len != func.params.len) {
-                return error.TypeSignatureMismatch;
-            }
+    var stack: [1 << 10]Op.Fixval = undefined;
+    const result = try self.run(&stack, func_id, converted_params[0..params.len]);
+    if (result) |res| {
+        return switch (func.result.?) {
+            .I32 => Value{ .I32 = res.I32 },
+            .I64 => Value{ .I64 = res.I64 },
+            .F32 => Value{ .F32 = res.F32 },
+            .F64 => Value{ .F64 = res.F64 },
+        };
+    } else {
+        return null;
+    }
+}
 
-            var converted_params: [params.len]Op.Fixval = undefined;
-            inline for ([_]void{{}} ** params.len) |_, i| {
-                const param_type: Module.Type.Value = switch (@TypeOf(params[i])) {
-                    i32, u32 => .I32,
-                    i64, u64 => .I64,
-                    f32 => .F32,
-                    f64 => .F64,
-                    else => @compileError("Unsupported type"),
-                };
-                if (param_type != func.params[i]) return error.TypeSignatureMismatch;
+fn run(self: *Instance, stack: []Op.Fixval, func_id: usize, params: []Op.Fixval) !?Op.Fixval {
+    var ctx = Execution.Context{
+        .memory = self.memory,
+        .funcs = self.funcs,
+        .allocator = self.allocator,
+        .jumps = self.module.jumps,
 
-                converted_params[i] = switch (@TypeOf(params[i])) {
-                    i32 => .{ .I32 = params[i] },
-                    i64 => .{ .I64 = params[i] },
-                    u32 => .{ .U32 = params[i] },
-                    u64 => .{ .U64 = params[i] },
-                    f32 => .{ .F32 = params[i] },
-                    f64 => .{ .F64 = params[i] },
-                    else => @compileError("Unsupported type"),
-                };
-            }
-
-            var stack: [1 << 10]Op.Fixval = undefined;
-            const result = try self.run(&stack, func_id, converted_params[0..params.len]);
-            if (result) |res| {
-                return switch (func.result.?) {
-                    .I32 => Value{ .I32 = res.I32 },
-                    .I64 => Value{ .I64 = res.I64 },
-                    .F32 => Value{ .F32 = res.F32 },
-                    .F64 => Value{ .F64 = res.F64 },
-                };
-            } else {
-                return null;
-            }
-        }
-
-        fn run(self: *Self, stack: []Op.Fixval, func_id: usize, params: []Op.Fixval) !?Op.Fixval {
-            var ctx = Execution.Context{
-                .memory = self.memory,
-                .funcs = self.funcs,
-                .allocator = self.allocator,
-                .jumps = self.module.jumps,
-
-                .stack = stack,
-                .stack_top = 0,
-            };
-            // Context may have grown the memory, so we need to copy the new memory in
-            // TODO: rearchitect so this copying is unnecessary
-            defer self.memory = ctx.memory;
-
-            // initCall assumes the params are already pushed onto the stack
-            for (params) |param| {
-                try ctx.push(Op.Fixval, param);
-            }
-
-            try ctx.initCall(func_id);
-
-            while (true) {
-                const func = ctx.funcs[ctx.current_frame.func];
-                // TODO: investigate imported calling another imported
-                if (ctx.current_frame.instr == 0 and func.kind == .imported) {
-                    const result = self.importCall(
-                        &ctx,
-                        func.kind.imported.module,
-                        func.kind.imported.field,
-                        ctx.getLocals(0, func.params.len),
-                    );
-
-                    _ = ctx.unwindCall();
-
-                    if (ctx.current_frame.isTerminus()) {
-                        std.debug.assert(ctx.stack_top == 0);
-                        return result;
-                    } else {
-                        if (result) |res| {
-                            ctx.push(Op.Fixval, res) catch unreachable;
-                        }
-                    }
-                } else if (ctx.current_frame.instr < func.kind.instrs.len) {
-                    const instr = func.kind.instrs[ctx.current_frame.instr];
-                    ctx.current_frame.instr += 1;
-
-                    ctx.stack_top -= instr.op.pop.len;
-                    const pop_array: [*]Op.Fixval = ctx.stack.ptr + ctx.stack_top;
-
-                    const result = try instr.op.step(&ctx, instr.arg, pop_array);
-                    if (result) |res| {
-                        try ctx.push(@TypeOf(res), res);
-                    }
-                } else {
-                    const result = ctx.unwindCall();
-
-                    if (ctx.current_frame.isTerminus()) {
-                        std.debug.assert(ctx.stack_top == 0);
-                        return result;
-                    } else {
-                        if (result) |res| {
-                            ctx.push(Op.Fixval, res) catch unreachable;
-                        }
-                    }
-                }
-            }
-        }
-
-        fn importCall(self: *Self, ctx: *Execution.Context, module: []const u8, field: []const u8, params: []const Op.Fixval) ?Op.Fixval {
-            inline for (std.meta.declarations(Imports)) |decl| {
-                if (std.mem.eql(u8, module, decl.name)) {
-                    inline for (std.meta.declarations(decl.data.Type)) |decl2| {
-                        if (std.mem.eql(u8, field, decl2.name)) {
-                            const func = @field(decl.data.Type, decl2.name);
-
-                            var args: std.meta.ArgsTuple(@TypeOf(func)) = undefined;
-                            std.debug.assert(@TypeOf(args[0]) == *Execution.Context);
-                            args[0] = ctx;
-                            inline for (std.meta.fields(@TypeOf(args))) |f, i| {
-                                if (i == 0) continue;
-
-                                switch (f.field_type) {
-                                    i32 => args[i] = params[i - 1].I32,
-                                    i64 => args[i] = params[i - 1].I64,
-                                    u32 => args[i] = params[i - 1].U32,
-                                    u64 => args[i] = params[i - 1].U64,
-                                    f32 => args[i] = params[i - 1].F32,
-                                    f64 => args[i] = params[i - 1].F64,
-                                    else => @panic("Signature not supported"),
-                                }
-                            }
-                            const fixval_len = std.math.divCeil(comptime_int, @sizeOf(@Frame(func)), @sizeOf(Op.Fixval)) catch unreachable;
-                            const frame_loc = ctx.pushOpaque(fixval_len) catch unreachable;
-                            const frame = @ptrCast(*@Frame(func), frame_loc);
-
-                            comptime const opts = std.builtin.CallOptions{ .modifier = .async_kw };
-                            frame.* = @call(opts, func, args);
-                            const result = nosuspend await frame;
-                            return switch (@TypeOf(result)) {
-                                i32 => Op.Fixval{ .I32 = result },
-                                i64 => Op.Fixval{ .I64 = result },
-                                u32 => Op.Fixval{ .U32 = result },
-                                u64 => Op.Fixval{ .U64 = result },
-                                f32 => Op.Fixval{ .F32 = result },
-                                f64 => Op.Fixval{ .F64 = result },
-                                else => @panic("Signature not supported"),
-                            };
-                        }
-                    }
-
-                    @panic("Func not found");
-                }
-            }
-
-            @panic("Module not found");
-        }
+        .stack = stack,
+        .stack_top = 0,
     };
+    // Context may have grown the memory, so we need to copy the new memory in
+    // TODO: rearchitect so this copying is unnecessary
+    defer self.memory = ctx.memory;
+
+    // initCall assumes the params are already pushed onto the stack
+    for (params) |param| {
+        try ctx.push(Op.Fixval, param);
+    }
+
+    try ctx.initCall(func_id);
+
+    while (true) {
+        const func = ctx.funcs[ctx.current_frame.func];
+        // TODO: investigate imported calling another imported
+        if (ctx.current_frame.instr == 0 and func.kind == .imported) {
+            const result = try func.kind.imported.func(&ctx, ctx.getLocals(0, func.params.len));
+
+            _ = ctx.unwindCall();
+
+            if (ctx.current_frame.isTerminus()) {
+                std.debug.assert(ctx.stack_top == 0);
+                return result;
+            } else {
+                if (result) |res| {
+                    ctx.push(Op.Fixval, res) catch unreachable;
+                }
+            }
+        } else if (ctx.current_frame.instr < func.kind.instrs.len) {
+            const instr = func.kind.instrs[ctx.current_frame.instr];
+            ctx.current_frame.instr += 1;
+
+            ctx.stack_top -= instr.op.pop.len;
+            const pop_array: [*]Op.Fixval = ctx.stack.ptr + ctx.stack_top;
+
+            const result = try instr.op.step(&ctx, instr.arg, pop_array);
+            if (result) |res| {
+                try ctx.push(@TypeOf(res), res);
+            }
+        } else {
+            const result = ctx.unwindCall();
+
+            if (ctx.current_frame.isTerminus()) {
+                std.debug.assert(ctx.stack_top == 0);
+                return result;
+            } else {
+                if (result) |res| {
+                    ctx.push(Op.Fixval, res) catch unreachable;
+                }
+            }
+        }
+    }
 }
 
 pub const Value = union(Module.Type.Value) {
@@ -280,20 +219,86 @@ pub const Export = union(enum) {
     Global: usize,
 };
 
+const ImportedFunc = struct {
+    func: fn (ctx: *Execution.Context, params: []const Op.Fixval) Op.WasmTrap!?Op.Fixval,
+    frame_size: usize,
+
+    fn init(comptime Imports: type, module: []const u8, field: []const u8) ImportedFunc {
+        inline for (std.meta.declarations(Imports)) |decl| {
+            if (decl.is_pub and std.mem.eql(u8, module, decl.name)) {
+                inline for (std.meta.declarations(decl.data.Type)) |decl2| {
+                    if (decl2.is_pub and std.mem.eql(u8, field, decl2.name)) {
+                        const func = @field(decl.data.Type, decl2.name);
+                        comptime const wrapped = wrap(func);
+                        return .{
+                            .func = wrapped,
+                            .frame_size = @frameSize(wrapped),
+                        };
+                    }
+                }
+
+                @panic("Func not found");
+            }
+        }
+
+        @panic("Module not found");
+    }
+
+    fn wrap(comptime func: anytype) fn (self: *Execution.Context, params: []const Op.Fixval) Op.WasmTrap!?Op.Fixval {
+        return (struct {
+            pub fn wrapped(ctx: *Execution.Context, params: []const Op.Fixval) Op.WasmTrap!?Op.Fixval {
+                var args: std.meta.ArgsTuple(@TypeOf(func)) = undefined;
+                std.debug.assert(@TypeOf(args[0]) == *Execution.Context);
+                args[0] = ctx;
+                inline for (std.meta.fields(@TypeOf(args))) |f, i| {
+                    if (i == 0) continue;
+
+                    switch (f.field_type) {
+                        i32 => args[i] = params[i - 1].I32,
+                        i64 => args[i] = params[i - 1].I64,
+                        u32 => args[i] = params[i - 1].U32,
+                        u64 => args[i] = params[i - 1].U64,
+                        f32 => args[i] = params[i - 1].F32,
+                        f64 => args[i] = params[i - 1].F64,
+                        else => @panic("Signature not supported"),
+                    }
+                }
+
+                // TODO: move async call to where this is being invoked
+                // const fixval_len = std.math.divCeil(comptime_int, @sizeOf(@Frame(func)), @sizeOf(Op.Fixval)) catch unreachable;
+                // const frame_loc = ctx.pushOpaque(fixval_len) catch unreachable;
+                // const frame = @ptrCast(*@Frame(func), frame_loc);
+                // comptime const opts = std.builtin.CallOptions{ .modifier = .async_kw };
+                // frame.* = @call(opts, func, args);
+                // const result = nosuspend await frame;
+
+                const result = @call(.{}, func, args);
+                return switch (@TypeOf(result)) {
+                    void => null,
+                    i32 => Op.Fixval{ .I32 = result },
+                    i64 => Op.Fixval{ .I64 = result },
+                    u32 => Op.Fixval{ .U32 = result },
+                    u64 => Op.Fixval{ .U64 = result },
+                    f32 => Op.Fixval{ .F32 = result },
+                    f64 => Op.Fixval{ .F64 = result },
+                    else => @panic("Signature not supported"),
+                };
+            }
+        }).wrapped;
+    }
+};
+
 pub const Func = struct {
     func_type: usize,
     params: []Module.Type.Value,
     result: ?Module.Type.Value,
     locals: []Module.Type.Value,
     kind: union(enum) {
-        imported: struct {
-            module: []const u8,
-            field: []const u8,
-        },
+        imported: ImportedFunc,
         instrs: []Module.Instr,
     },
 };
 
 test "" {
-    _ = Instance(struct {}).call;
+    _ = call;
 }
