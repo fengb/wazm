47d4e8f2c4d5e04fc1e3f8f64c2ef633f8dd8c04
Benjamin Feng <benjamin.feng@glassdoor.com>
2021-06-08 07:41:21 -0500

    Add standardTargetOptions

--- build.zig
+++ build.zig
@@ -1,8 +1,10 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
+    const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
     const exe = b.addExecutable("wazm", "src/main.zig");
+    exe.setTarget(target);
     exe.setBuildMode(mode);
     exe.install();
 
